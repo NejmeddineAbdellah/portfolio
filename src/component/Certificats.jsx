5c2e9f8b974ccdfd316b3b76ef9f06c286784b21
abdellah nejmeddine <crissnajm1998@gmail.com>
2023-04-24 15:55:52 +0100

    V fianl

--- src/component/Certificats.jsx
+++ src/component/Certificats.jsx
@@ -4,16 +4,14 @@ import { Card } from 'primereact/card';
 
 function Certification() {
   const office = (
-    <img alt="Card" src={require('../images/Cert9792259747.jpg')} style={{ heigh:300 }} />
+    <img alt="Card" src={require('../images/Cert9792259747.jpg')} style={{ heigh:'250px' }} />
   );
   const react = (
-    <img alt="Card" src={require('../images/Coursera FullStackWeb.jpg')} style={{ height: 300 }} />
+    <img alt="Card" src={require('../images/Coursera FullStackWeb.jpg')} style={{ height:'250px' }} />
   );  
   const devops = (
-    <img alt="Card" src={require('../images/Devops.jpg')} style={{ height: 300 }} />
+    <img alt="Card" src={require('../images/Devops.jpg')} style={{ height:'250px'}} />
   );
-
-
   return (
     <div class='container w-100'>
         <div class='project-container row' style={{ marginTop: 40 }}>
@@ -33,7 +31,6 @@ function Certification() {
             </Card>
           </div>
 
-
           <div className='project-card col-lg-4 col-md-4 col-sm-12 mb-4'>
             <Card header={devops} className="card" >
               <div className='project-title card-title h5' style={{ textAlign: "left" }}>
@@ -49,13 +46,11 @@ function Certification() {
             </Card>
           </div>
 
-          
           <div className='project-card col-lg-4 col-md-4 col-sm-12 mb-4'>
             <Card header={react} className="card" >
               <div className='project-title card-title h5' style={{ textAlign: "left" }}>
                 Full-Stack Web Development with React
               </div>
-              <br/>
               <br/>
               <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
                  IBM
--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,97 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Carousel from "react-elastic-carousel";
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
-import { Link } from "react-router-dom";
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
-
-const breakPoints = [
-  { width: 400, itemsToShow: 1 },
-  { width: 800, itemsToShow: 2 },
-  { width: 1080, itemsToShow: 3 },
-];
-const glib = (
-  <img alt="Card" src={require('../images/project/gestion_librairie.png')} style={{ height: 250 }} />
-);
-const smarthome = (
-  <img alt="Card" src={require('../images/project/smarthome.png')} style={{ height: 250 }} />
-);
-const pok1 = (
-  <img alt="Card" src={require('../images/project/pok1 (1).png')} style={{ height: 250 }} />
-);
-const pok2 = (
-  <img alt="Card" src={require('../images/project/pok1 (2).png')} style={{ height: 250 }} />
-);
-const tic = (
-  <img alt="Card" src={require('../images/project/tictactoe.png')} style={{ height: 250 }} />
-);
 export default function Projet() {
 
+
+  const tic = (
+    <img alt="Card" src={require('../images/project/tictactoe.png')} style={{ height: 300 }} />
+  );
+  const pok = (
+    <img alt="Card" src={require('../images/project/pok1 (1).png')} style={{ height: 300 }} />
+  );
+  const lib = (
+    <img alt="Card" src={require('../images/project/gestion_librairie.png')} style={{ height: 300 }} />
+  );
+  const smart = (
+    <img alt="Card" src={require('../images/project/smarthome.png')} style={{ height: 300 }} />
+  );
   return (
+    <div class='container w-100'>
+      <div class='project-container row' style={{ marginTop: 40 }}>
 
-    <div className="App" style={{ marginTop: 40 }}>
-      <Carousel breakPoints={breakPoints} className='project-container rows'>
-        <div className='project-card col-12 col-md-10 mb-4'>
-          <Card header={smarthome} className="card" >
-            <div className='project-title card-title h5' style={{ textAlign: "left" }}>
-              Smarthome application 
-            </div>
-            <br />
-            <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
-              C#
-            </div>
-            <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
-              <br />
-              <Link to="https://github.com/NejmeddineAbdellah/Smarthome">
-                <Button  icon="pi pi-search" size="small" aria-label="Filter" label="Voir le projet" severity="success" />
-              </Link>
-            </div>
-
-          </Card>
-        </div>
-        <div className='project-card col-12 col-md-10 mb-4'>
-          <Card header={glib} className="card" >
-            <div className='project-title card-title h5' style={{ textAlign: "left" }}>
-              Desktop application Gestion_Librairie 
-            </div>
-            <br />
-            <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
-              C#, LinqToSql
-
-            </div>
-            <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
-              <br />
-              <Link to="https://github.com/NejmeddineAbdellah/Gestion_Librairie">
-              <Button  icon="pi pi-search" size="small" aria-label="Filter" label="Voir le projet" severity="success" />
-              </Link>
-
-            </div>
-
-          </Card>
-        </div>
-        <div className='project-card col-12 col-md-10 mb-4'>
-          <Card header={pok1} className="card" >
-            <div className='project-title card-title h5' style={{ textAlign: "left" }}>
-              Pokemon Application APi mobile version
-            </div>
-            <br />
-            <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
-              JAVA - android
-            </div>
-            <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
-              <br />
-              <Link to="https://github.com/NejmeddineAbdellah/pokemon">
-              <Button  icon="pi pi-search" size="small" aria-label="Filter" label="Voir le projet" severity="success" />
-              </Link>
-            </div>
-
-          </Card>
-        </div>
-        <div className='project-card col-12 col-md-10 mb-4'>
+        <div className='project-card col-lg-4 col-md-4 col-sm-12 mb-4'>
           <Card header={tic} className="card" >
             <div className='project-title card-title h5' style={{ textAlign: "left" }}>
-            Tic-Tac-Toe Game
+              Tic-Tac-Toe game
             </div>
             <br />
             <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
@@ -99,16 +35,76 @@ export default function Projet() {
             </div>
             <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
               <br />
-              <Link to="https://github.com/NejmeddineAbdellah/tictactoe" >
-              <Button  icon="pi pi-search" size="small" aria-label="Filter" label="Voir le projet" severity="success" />
-              </Link>
-              
-            </div>
 
+              <Button label="Code source" severity="success" >
+                <a href="https://github.com/NejmeddineAbdellah/tictactoe" target="_blank" rel="noopener noreferrer"></a>
+              </Button>
+            </div>
           </Card>
         </div>
-      </Carousel>
-    </div>
 
+        <div className='project-card col-lg-4 col-md-4 col-sm-12 mb-4'>
+          <Card header={pok} className="card" >
+            <div className='project-title card-title h5' style={{ textAlign: "left" }}>
+              Application Android Pokemon API
+            </div>
+            <br />
+            <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
+              JAVA api
+            </div>
+            <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
+              <br />
+
+              <Button label="Code source" severity="success" >
+                <a href="https://github.com/NejmeddineAbdellah/pokemon" target="_blank" rel="noopener noreferrer"></a>
+              </Button>
+            </div>
+          </Card>
+        </div>
+
+        <div className='project-card col-lg-4 col-md-4 col-sm-12 mb-4'>
+          <Card header={lib} className="card" >
+            <div className='project-title card-title h5' style={{ textAlign: "left" }}>
+              Application Gestion librairie
+            </div>
+            <br />
+            <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
+              C#
+            </div>
+            <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
+              <br />
+
+              <Button label="Code source" severity="success" >
+                <a href="https://github.com/NejmeddineAbdellah/Gestion_Librairie" target="_blank" rel="noopener noreferrer"></a>
+              </Button>
+            </div>
+          </Card>
+        </div>
+
+        <div className='project-card col-lg-4 col-md-4 col-sm-12 mb-4'>
+          <Card header={smart} className="card" >
+            <div className='project-title card-title h5' style={{ textAlign: "left" }}>
+              Smarthome desktop application
+            </div>
+            <br />
+            <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
+              C#
+            </div>
+            <div className='project-school card-subtitle h6' style={{ textAlign: "left" }}>
+              <br />
+              <Button label="Code source" severity="success" >
+                <a href="https://github.com/NejmeddineAbdellah/smarthome" target="_blank" rel="noopener noreferrer"></a>
+              </Button>
+            </div>
+          </Card>
+        </div>
+
+      </div>
+    </div>
   );
+
+
+
+
+
 }
